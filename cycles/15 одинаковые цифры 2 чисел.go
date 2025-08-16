8bb12a3a498caf0aa99a13c029512a1577d10107
silaryblia <volodiamarkiv@gmail.com>
2025-08-16 17:43:28 +0300

    hello

---
+++ cycles/15 одинаковые цифры 2 чисел.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+// Найти одинаковые цифры двух чисел
+
+func main() {
+	var a, b int
+
+	fmt.Println("Введите два числа: ")
+	fmt.Scan(&a, &b)
+
+	fmt.Print("Общие цифры: ")
+
+	for digit := 0; digit <= 9; digit++ {
+		foundInA := false
+		foundInB := false
+
+		for temp := a; temp > 0; temp /= 10 {
+			if temp%10 == digit {
+				foundInA = true
+				break
+			}
+		}
+
+		for temp := b; temp > 0; temp /= 10 {
+			if temp%10 == digit {
+				foundInB = true
+				break
+			}
+		}
+
+		if foundInA && foundInB {
+			fmt.Print(digit, " ")
+		}
+	}
+	fmt.Println()
+
+}
